12190ae2e0d2982e8a3dfa9efae347af749c4000
norrico31 <norricobiason31@gmail.com>
2023-03-19 21:40:34 +0800

    feature: login ui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-import { Form, Input, Checkbox, Button, Row, Col } from 'antd'
+import { Form, Input, Checkbox, Button, Row, Col, Typography } from 'antd'
 import { Navigate } from 'react-router-dom'
-import axiosClient from '../shared/utils/axios'
 import { useAuthContext } from '../shared/contexts/Auth'
+import axiosClient from '../shared/utils/axios'
+import ImgBG from '../shared/assets/bg-login.png'
+import RcsLogo from '../shared/assets/logo.png'
 
 export default function Login() {
     const { token, setToken } = useAuthContext()
@@ -27,46 +29,60 @@ export default function Login() {
     }
 
     return (
-        <Row justify='center' style={{ minHeight: '100vh', width: '100%', }} align='middle'>
-            <Col xs={18} sm={18} md={18} lg={18} xl={11} style={{ outline: '1px solid lime', height: 630 }} >
-                {error}
-                <Form
-                    autoComplete='off'
-                    layout='vertical'
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your email!",
-                            },
-                        ]}
+        <div>
+            <Row justify='center' style={{ minHeight: '100vh', width: '100%', }} align='middle'>
+                <Col xs={18} sm={18} md={18} lg={18} xl={11} style={{ outline: '1px solid lime', height: 630 }} >
+                    {error}
+                    <Form
+                        autoComplete='off'
+                        layout='vertical'
+                        onFinish={onFinish}
                     >
-                        <Input type='email' placeholder='Enter email' />
-                    </Form.Item>
-                    <Form.Item
-                        label="Password"
-                        name="password"
+                        <Row justify='center' align='middle'>
+                            <Col>
+                                <div className='center' style={style}>
+                                    <img src={RcsLogo} alt='logo' />
+                                    <Typography.Title level={2} color='#E49944'>Login</Typography.Title>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please input your email!",
+                                },
+                            ]}
+                        >
+                            <Input type='email' placeholder='Enter email' />
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            name="password"
 
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your password!",
-                            },
-                        ]}
-                    >
-                        <Input.Password type='password' placeholder='Enter password' />
-                    </Form.Item>
-                    <Form.Item style={{ textAlign: 'center' }}>
-                        <Button type="primary" htmlType="submit">
-                            Login
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Col>
-        </Row>
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please input your password!",
+                                },
+                            ]}
+                        >
+                            <Input.Password type='password' placeholder='Enter password' />
+                        </Form.Item>
+                        <Form.Item style={{ textAlign: 'center' }}>
+                            <Button type="primary" htmlType="submit">
+                                Login
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </div>
     );
+}
+
+const style: Record<string, string> = {
+    '--gap': '1rem'
 }
